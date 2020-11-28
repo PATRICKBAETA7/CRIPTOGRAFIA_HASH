e250a61c5920175008c6468f3d53c82e0e7b1639
Patrick Junior Nascimento Baeta <34289541+PATRICKBAETA7@users.noreply.github.com>
2020-11-27 23:53:12 -0300

    Update ContaLogado.Designer.cs

--- WindowsFormsApp/ContaLogado.Designer.cs
+++ WindowsFormsApp/ContaLogado.Designer.cs
@@ -30,16 +30,50 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContaLogado));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btFechar = new System.Windows.Forms.Button();
+            this.btMenuPrincipal = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(257, 493);
+            this.button1.Location = new System.Drawing.Point(250, 351);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(155, 37);
             this.button1.TabIndex = 0;
-            this.button1.Text = "BEM-VINDO! AGUARDE O CARREGAMENTO.";
+            this.button1.Text = "BEM-VINDO! ";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(597, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LOG OFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btFechar
+            // 
+            this.btFechar.Location = new System.Drawing.Point(597, 113);
+            this.btFechar.Name = "btFechar";
+            this.btFechar.Size = new System.Drawing.Size(75, 23);
+            this.btFechar.TabIndex = 2;
+            this.btFechar.Text = "FECHAR";
+            this.btFechar.UseVisualStyleBackColor = true;
+            this.btFechar.Click += new System.EventHandler(this.btFechar_Click);
+            // 
+            // btMenuPrincipal
+            // 
+            this.btMenuPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMenuPrincipal.Location = new System.Drawing.Point(597, 79);
+            this.btMenuPrincipal.Name = "btMenuPrincipal";
+            this.btMenuPrincipal.Size = new System.Drawing.Size(75, 23);
+            this.btMenuPrincipal.TabIndex = 3;
+            this.btMenuPrincipal.Text = "PRINCIPAL";
+            this.btMenuPrincipal.UseVisualStyleBackColor = true;
+            this.btMenuPrincipal.Click += new System.EventHandler(this.btMenuPrincipal_Click);
             // 
             // ContaLogado
             // 
@@ -47,12 +81,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(704, 599);
+            this.Controls.Add(this.btMenuPrincipal);
+            this.Controls.Add(this.btFechar);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.Name = "ContaLogado";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ContaLogado";
-            this.Load += new System.EventHandler(this.ContaLogado_Load);
             this.ResumeLayout(false);
 
         }
@@ -60,5 +96,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btFechar;
+        private System.Windows.Forms.Button btMenuPrincipal;
     }
 }
